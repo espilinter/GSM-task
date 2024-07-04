d4c248aa65c4b9d782cf8e117f73e057dfffd9ec
espilinter <FARSHAD.fsn@gmail.com>
2024-07-04 19:43:55 +0330

    handle add task & move tasks & delete tasks

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,65 @@
+'use client'
 import Image from 'next/image'
 import trash from "@/public/images/trash.png"
 import circle from "@/public/images/circle.png"
 import fill from "@/public/images/fill.png"
 import back from "@/public/images/return.png"
+import { useRef, useState } from 'react'
 
 export default function Home() {
+  interface taskType {
+    id: string;
+    text: string;
+    done: boolean;
+  }
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [tasks, setTasks] = useState<taskType[]>([])
+  function addTask() {
+    if (inputRef.current) {
+      console.log(inputRef.current.value);
+      const object: taskType = { id: inputRef.current.value, text: inputRef.current.value, done: false }
+      setTasks([...tasks, object])
+    }
+  }
+
+  function taskDone(id: string) {
+    let object = [...tasks]
+    tasks.map((item, index) => {
+      if (item.id === id) {
+        object[index].done = true;
+      }
+    })
+    setTasks(object)
+  }
+
+  function taskNotDone(id: string) {
+    let object = [...tasks]
+    tasks.map((item, index) => {
+      if (item.id === id) {
+        object[index].done = false;
+      }
+    })
+    setTasks(object)
+  }
+
+  function deleteHandler(id: string) {
+    let object = [...tasks]
+    tasks.map((item, index) => {
+      if (item.id === id) {
+        object.splice(index, 1)
+      }
+    })
+    setTasks(object)
+  }
+
   return (
     <main className="flex flex-col items-center my-20 m-auto gap-8 max-w-[1024px] w-full">
       <div className='flex flex-row gap-8 items-start w-full'>
         <div className='flex border rounded-lg flex-col gap-4 p-3 w-[calc(66%-16px)]'>
           <span className='text-lg font-bold'>افزودن تسک جدید</span>
           <div className='flex gap-2'>
-            <input className='border border-neutral-400 rounded-md p-1 w-full' placeholder='امروز میخوام...' />
-            <button className='rounded-md bg-purple-900 px-4 py-1 text-white'>افزودن</button>
+            <input className='border border-neutral-400 rounded-md p-1 w-full' placeholder='امروز میخوام...' ref={inputRef} />
+            <button className='rounded-md bg-purple-900 px-4 py-1 text-white' onClick={addTask}>افزودن</button>
           </div>
         </div>
         <div className='flex border rounded-lg flex-col gap-4 p-3 w-[calc(33%-16px)]'>
@@ -22,7 +69,16 @@ export default function Home() {
       <div className='flex border rounded-lg flex-col gap-4 p-3 w-full'>
         <span className='text-lg font-bold'>درحال انجام</span>
         <div className='flex flex-col'>
-          <div className='flex justify-between items-center [&:not(:last-child)]:border-b p-3'>
+          {tasks.map((item) => (
+            <div className={`flex justify-between items-center [&:not(:last-child)]:border-b p-3 ${item.done === true ? "hidden" : ""}`}>
+              <div className='flex gap-2 items-center' onClick={() => { taskDone(item.id) }}>
+                <Image className='' src={circle} alt='circle icon' width={16} height={16} />
+                <span className=''>{item.text}</span>
+              </div>
+              <div className='rounded-[4px] border size-6 flex items-center justify-center' id={item.id} onClick={() => { deleteHandler(item.id) }}><Image src={trash} alt='trash icon' width={16} height={16} /></div>
+            </div>
+          ))}
+          {/* <div className='flex justify-between items-center [&:not(:last-child)]:border-b p-3'>
             <div className='flex gap-2 items-center'>
               <Image className='' src={circle} alt='circle icon' width={16} height={16} />
               <span className=''>انجام تسک اول</span>
@@ -35,14 +91,7 @@ export default function Home() {
               <span className=''>انجام تسک اول</span>
             </div>
             <div className='rounded-[4px] border size-6 flex items-center justify-center'><Image src={trash} alt='trash icon' width={16} height={16} /></div>
-          </div>
-          <div className='flex justify-between items-center [&:not(:last-child)]:border-b p-3'>
-            <div className='flex gap-2 items-center'>
-              <Image className='' src={circle} alt='circle icon' width={16} height={16} />
-              <span className=''>انجام تسک اول</span>
-            </div>
-            <div className='rounded-[4px] border size-6 flex items-center justify-center'><Image src={trash} alt='trash icon' width={16} height={16} /></div>
-          </div>
+          </div> */}
         </div>
         <div className='bg-neutral-200 p-2 rounded-md flex items-center justify-between'>
           <p className='text-neutral-800 text-sm'>00:09 ثانیه برای بازگرداندن فرصت دارید</p>
@@ -52,12 +101,14 @@ export default function Home() {
       <div className='flex border rounded-lg flex-col gap-4 p-3 w-full'>
         <span className='text-lg font-bold'>انجام شده</span>
         <div className='flex flex-col'>
-          <div className='flex justify-between items-center [&:not(:last-child)]:border-b p-3'>
-            <div className='flex gap-2 items-center'>
-              <Image src={fill} alt='circle-fill icon' width={16} height={16} />
-              <span className='line-through'>انجام تسک اول</span>
+          {tasks.map((item) => (
+            <div className={`flex justify-between items-center [&:not(:last-child)]:border-b p-3 ${item.done === true ? "" : "hidden"}`} onClick={() => { taskNotDone(item.id) }}>
+              <div className='flex gap-2 items-center'>
+                <Image className='' src={fill} alt='circle icon' width={16} height={16} />
+                <span className=''>{item.text}</span>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </main>
