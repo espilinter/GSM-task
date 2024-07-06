489868f00b6d469f61b42f7552601ef9839e1938
espilinter <FARSHAD.fsn@gmail.com>
2024-07-06 20:00:16 +0330

    add label to PieChart

--- components/SuccessChart.tsx
+++ components/SuccessChart.tsx
@@ -4,16 +4,40 @@ interface propsType {
     completed: number,
     pending: number
 }
+
+interface labelType {
+    cx: number,
+    cy: number,
+    midAngle: number,
+    innerRadius: number,
+    outerRadius: number,
+    percent: number,
+    index: number,
+}
 const SuccessChart: React.FC<propsType> = ({ completed, pending }) => {
     const data = [
         { name: 'انجام شده', value: completed, color: '#1f1f46' },
         { name: 'در حال انجام', value: pending, color: '#28af65' },
     ];
 
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel: React.FC<labelType> = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+        return (
+            <text x={x + 10} y={y + 2} fill="white" textAnchor={"center"} alignmentBaseline='middle' className='text-[10px]'>
+                {`${(percent * 100).toFixed(0)}%`}
+            </text>
+        );
+    };
+
     return (
         <div className='text-center'>
             <PieChart width={100} height={60} >
                 <Pie
+                    label={renderCustomizedLabel}
                     data={data}
                     dataKey="value"
                     cx="50%"
@@ -24,6 +48,7 @@ const SuccessChart: React.FC<propsType> = ({ completed, pending }) => {
                     endAngle={0}
                     fill="#8884d8"
                     paddingAngle={2}
+                    labelLine={false}
                 >
                     {data.map((entry, index) => (
                         <Cell key={`cell-${index}`} fill={entry.color} />
